cc2c15ff939c2dacd937ceee46ac7a9691f13eb2
Thiago <tsg.gomes9@gmail.com>
2026-02-19 23:47:03 -0400

    feat: style checkpoint system

--- src/app/admin/concursos/[id]/materias/page.tsx
+++ src/app/admin/concursos/[id]/materias/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import { useState, useEffect, JSX } from "react";
 import { useParams, useRouter } from "next/navigation";
 import {
   doc,
